f873c15ba2b3da10fc6cffeed7e56ea7e4e13f6d
F5BDE6 <f5bde6@proton.me>
2025-08-19 22:07:20 +0100

    Fixed all copper tools incorrectly created as Pickaxe

--- src/main/java/net/hellomouse/progression_change/registries/ProgressionModItemRegistry.java
+++ src/main/java/net/hellomouse/progression_change/registries/ProgressionModItemRegistry.java
@@ -15,19 +15,19 @@ public class ProgressionModItemRegistry {
     public static final DeferredRegister<Item> VANILLA_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "minecraft");
     public static final DeferredRegister<Item> DEF_REG = DeferredRegister.create(ForgeRegistries.ITEMS, ProgressionMod.MODID);
     public static final RegistryObject<Item> COPPER_SWORD = DEF_REG.register("copper_sword", () ->
-            new PickaxeItem(ProgressionModToolMaterials.COPPER, 3, -2.4f, new Item.Settings()) // The attackDamage is actually 1.5 in the snapshot
+            new SwordItem(ProgressionModToolMaterials.COPPER, 3, -2.4f, new Item.Settings()) // The attackDamage is actually 1.5 in the snapshot
     );
     public static final RegistryObject<Item> COPPER_SHOVEL = DEF_REG.register("copper_shovel", () ->
-            new PickaxeItem(ProgressionModToolMaterials.COPPER, 1, -2.0f, new Item.Settings()) // The attackDamage is actually 1.5 in the snapshot
+            new ShovelItem(ProgressionModToolMaterials.COPPER, 1, -2.0f, new Item.Settings()) // The attackDamage is actually 1.5 in the snapshot
     );
     public static final RegistryObject<Item> COPPER_PICKAXE = DEF_REG.register("copper_pickaxe", () ->
             new PickaxeItem(ProgressionModToolMaterials.COPPER, 1, -2.8f, new Item.Settings())
     );
     public static final RegistryObject<Item> COPPER_AXE = DEF_REG.register("copper_axe", () ->
-            new PickaxeItem(ProgressionModToolMaterials.COPPER, 7, -3.2f, new Item.Settings())
+            new AxeItem(ProgressionModToolMaterials.COPPER, 7, -3.2f, new Item.Settings())
     );
     public static final RegistryObject<Item> COPPER_HOE = DEF_REG.register("copper_hoe", () ->
-            new PickaxeItem(ProgressionModToolMaterials.COPPER, -1, -2.0f, new Item.Settings())
+            new HoeItem(ProgressionModToolMaterials.COPPER, -1, -2.0f, new Item.Settings())
     );
     public static final RegistryObject<Item> COPPER_HELMET = DEF_REG.register("copper_helmet", () ->
             new ArmorItem(ProgressionModArmourMaterials.COPPER, ArmorItem.Type.HELMET, new Item.Settings())
@@ -85,7 +85,10 @@ public class ProgressionModItemRegistry {
             RAW_IRON_NUGGET,
             RAW_GOLD_NUGGET,
             DIAMOND_FRAGMENT,
-            RAW_BRICK);
+            RAW_BRICK,
+            FLINT_HATCHET,
+            PLANT_FIBER
+    );
 
     public static void addItemToCreativeTab(ItemGroup.Entries entries) {
         for (var i : ProgressionModItemRegistry.CREATIVE_MOD_TAB) {
