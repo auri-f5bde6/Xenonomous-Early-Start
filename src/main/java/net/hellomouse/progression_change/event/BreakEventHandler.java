b0b54650e76ebb5036d89f21e7f50c8972b473de
F5BDE6 <f5bde6@proton.me>
2025-08-20 21:21:20 +0100

    Readded code to hijack onBreakEvent, as they're needed for stone->cobblestone behaviour, perfered for plant->plant fiber and log changes

---
+++ src/main/java/net/hellomouse/progression_change/event/BreakEventHandler.java
@@ -0,0 +1,110 @@
+package net.hellomouse.progression_change.event;
+
+import net.hellomouse.progression_change.ProgressionMod;
+import net.hellomouse.progression_change.ProgressionModConfig;
+import net.hellomouse.progression_change.ProgressionModTags;
+import net.hellomouse.progression_change.ProgressionModToolMaterials;
+import net.hellomouse.progression_change.registries.ProgressionModItemRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.TierSortingRegistry;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod.EventBusSubscriber(modid = ProgressionMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class BreakEventHandler {
+    @SubscribeEvent
+    public static void onBreakEvent(BlockEvent.BreakEvent event) {
+        var eventState = event.getState();
+        var level = event.getLevel();
+        var pos = event.getPos();
+        var player = event.getPlayer();
+        var toolStack = player.getMainHandStack();
+        var state = event.getState();
+        if (eventState.canHarvestBlock(level, pos, player)) {
+            if (toolStack.isIn(ProgressionModTags.Items.SHARDS)) {
+                if (state.isIn(BlockTags.LEAVES) || state.isIn(BlockTags.REPLACEABLE_BY_TREES)) {
+                    // I'm assuming that REPLACEABLE_BY_TREES blocks are either grass or flower, I hope I didn't make Ming looks bad
+                    if (level.getRandom().nextFloat() < (ProgressionModConfig.earlyGameChanges.plantFiberDropProbability / 100f)) {
+                        Block.dropStack((World) level, pos, ProgressionModItemRegistry.PLANT_FIBER.get().getDefaultStack());
+                        return;
+                    }
+                }
+            }
+            if (state.isIn(BlockTags.LOGS) && !(toolStack.getItem() instanceof AxeItem)) {
+                breakBlock(toolStack, level, player, state, pos);
+                event.setCanceled(true);
+                return;
+            }
+            // Turn deepslate into cobbled deepslate, modded stone can turn into cobblestone for now
+            if (shouldReplaceDrop(toolStack, ProgressionModToolMaterials.COPPER)) {
+                var drop = level.getRandom().nextFloat() < (ProgressionModConfig.earlyGameChanges.pebbleDropProbability / 100f);
+                if (state.isOf(Blocks.BLACKSTONE)) {
+                    if (drop) {
+                        replaceDrop(event, toolStack, level, player, state, pos, ProgressionModItemRegistry.BLACKSTONE_PEBBLE.get().getDefaultStack());
+                    } else {
+                        breakBlock(toolStack, level, player, state, pos);
+                    }
+                    event.setCanceled(true);
+                } else if (state.isOf(Blocks.DEEPSLATE)) {
+                    if (drop) {
+                        replaceDrop(event, toolStack, level, player, state, pos, ProgressionModItemRegistry.DEEPSLATE_PEBBLE.get().getDefaultStack());
+                    } else {
+                        breakBlock(toolStack, level, player, state, pos);
+                    }
+                    level.setBlockState(pos, Blocks.COBBLED_DEEPSLATE.getDefaultState(), Block.NOTIFY_ALL);
+                    event.setCanceled(true);
+                } else if (state.isIn(Tags.Blocks.STONE) && !state.isOf(Blocks.DEEPSLATE)) {
+                    if (drop) {
+                        replaceDrop(event, toolStack, level, player, state, pos, ProgressionModItemRegistry.PEBBLE.get().getDefaultStack());
+                    } else {
+                        breakBlock(toolStack, level, player, state, pos);
+                    }
+                    level.setBlockState(pos, Blocks.COBBLESTONE.getDefaultState(), Block.NOTIFY_ALL);
+                    event.setCanceled(true);
+                }
+            }
+        }
+    }
+
+    private static boolean shouldReplaceDrop(ItemStack toolStack, ToolMaterial lowerThanTier) {
+        if (toolStack.getItem() instanceof MiningToolItem toolItem) {
+            var toolMaterial = toolItem.getMaterial();
+            return (toolMaterial.getMiningLevel() < lowerThanTier.getMiningLevel() && ProgressionModConfig.oreDropChanges.moddedPickaxeWorkaround)
+                    || TierSortingRegistry.getTiersLowerThan(lowerThanTier).contains(toolMaterial);
+        } else {
+            return false;
+        }
+
+    }
+
+    private static void replaceDrop(BlockEvent.BreakEvent event, ItemStack toolStack, WorldAccess level, PlayerEntity player, BlockState blockState, BlockPos pos, ItemStack toDrop) {
+        breakBlock(toolStack, level, player, blockState, pos);
+        Block.dropStack((World) level, pos, toDrop);
+        var exp = event.getExpToDrop();
+        if (exp > 0) {
+            blockState.getBlock().dropExperience((ServerWorld) level, pos, exp);
+        }
+        event.setCanceled(true);
+    }
+
+    private static void breakBlock(ItemStack toolStack, WorldAccess level, PlayerEntity player, BlockState blockState, BlockPos pos) {
+        level.breakBlock(pos, false, player);
+        blockState.getBlock().onBroken(level, pos, blockState);
+        toolStack.postMine((World) level, blockState, pos, player);
+    }
+}
