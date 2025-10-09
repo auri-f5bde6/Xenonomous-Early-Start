5ef041175acdf6a7400eb42c27f0bfa78e5dbf61
Auri <f5bde6@proton.me>
2025-10-10 00:52:22 +0100

    Brick drying sounds and particle

--- src/main/kotlin/net/hellomouse/xeno_early_start/block/RawBrickBlock.kt
+++ src/main/kotlin/net/hellomouse/xeno_early_start/block/RawBrickBlock.kt
@@ -8,11 +8,15 @@ import net.minecraft.entity.Entity
 import net.minecraft.entity.ItemEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.particle.ParticleTypes
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
 import net.minecraft.state.StateManager
 import net.minecraft.state.property.IntProperty
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
+import net.minecraft.util.math.Vec3d
 import net.minecraft.util.math.random.Random
 import net.minecraft.world.World
 
@@ -24,6 +28,30 @@ class RawBrickBlock(arg: Settings) : BrickBlock(arg) {
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
         super.appendProperties(builder)
         builder.add(DRYING_LEVEL)
+    }
+
+    override fun randomDisplayTick(state: BlockState, world: World, pos: BlockPos, rand: Random) {
+        val particlePos = Vec3d.of(pos)
+            .add(0.5 + (rand.nextFloat() - 0.5) * 0.2, rand.nextFloat() * 0.4, 0.5 + (rand.nextFloat() - 0.5) * 0.2)
+        world.addParticle(
+            ParticleTypes.POOF,
+            particlePos.x,
+            particlePos.y,
+            particlePos.z,
+            0.0,
+            0.0,
+            0.0
+        )
+        world.playSound(
+            pos.x + 0.5,
+            pos.y + 0.5,
+            pos.z + 0.5,
+            SoundEvents.BLOCK_FIRE_EXTINGUISH,
+            SoundCategory.BLOCKS,
+            0.1f + rand.nextFloat() * 0.1f,
+            rand.nextFloat() * 0.7f + 0.6f,
+            false
+        )
     }
 
     @Deprecated("Deprecated in Java, I guess")
