b7efffa337c9c5a48c7eb823d388622282d31c0c
Auri <f5bde6@proton.me>
2025-10-31 14:38:49 +0000

    Squid now attack player when attacked/when player is weak

---
+++ src/main/java/net/hellomouse/xeno_early_start/mixins/mob_changes/SquidEntityMixin.java
@@ -0,0 +1,103 @@
+package net.hellomouse.xeno_early_start.mixins.mob_changes;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.hellomouse.xeno_early_start.utils.OtherUtils;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.mob.Angerable;
+import net.minecraft.entity.mob.WaterCreatureEntity;
+import net.minecraft.entity.passive.SquidEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.TimeHelper;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
+import java.util.UUID;
+
+@Mixin(SquidEntity.class)
+public abstract class SquidEntityMixin extends WaterCreatureEntity implements Angerable {
+    @Unique
+    private static final UniformIntProvider ANGER_TIME_RANGE = TimeHelper.betweenSeconds(20, 39);
+    @Unique
+    private int xeno_early_start$angerTime;
+    @Unique
+    @Nullable
+    private UUID xeno_early_start$angryAt;
+
+    protected SquidEntityMixin(EntityType<? extends WaterCreatureEntity> arg, World arg2, int angerTime, @Nullable UUID angryAt) {
+        super(arg, arg2);
+        this.xeno_early_start$angerTime = angerTime;
+        this.xeno_early_start$angryAt = angryAt;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T unchecked(Object o) {
+        return (T) o;
+    }
+
+    @Inject(method = "initGoals", at = @At("TAIL"))
+    private void initGoals(CallbackInfo ci) {
+        this.targetSelector.add(0, new RevengeGoal(this));
+        this.targetSelector.add(1, new ActiveTargetGoal<>((SquidEntity) (Object) this, PlayerEntity.class, 10, true, false,
+                livingEntity -> this.shouldAngerAt(livingEntity) || OtherUtils.isLivingEntityWeak(livingEntity))
+        );
+        this.targetSelector.add(2, new UniversalAngerGoal<>(unchecked(this), true));
+    }
+
+    @WrapOperation(method = "initGoals", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/goal/GoalSelector;add(ILnet/minecraft/entity/ai/goal/Goal;)V", ordinal = 1))
+    private void doNotRun(GoalSelector instance, int priority, Goal goal, Operation<Void> original) {
+        this.goalSelector.add(1, new MeleeAttackGoal(this, 1, false));
+    }
+
+    @Override
+    public void chooseRandomAngerTime() {
+        this.setAngerTime(ANGER_TIME_RANGE.get(this.random));
+    }
+
+    @Override
+    public int getAngerTime() {
+        return this.xeno_early_start$angerTime;
+    }
+
+    @Override
+    public void setAngerTime(int angerTime) {
+        this.xeno_early_start$angerTime = angerTime;
+    }
+
+    @Nullable
+    @Override
+    public UUID getAngryAt() {
+        return this.xeno_early_start$angryAt;
+    }
+
+    @Override
+    public void setAngryAt(@Nullable UUID angryAt) {
+        this.xeno_early_start$angryAt = angryAt;
+    }
+
+    @Override
+    public boolean tryAttack(Entity target) {
+        boolean bl = target.damage(this.getDamageSources().mobAttack(this), 10);
+        if (bl) {
+            this.applyDamageEffects(this, target);
+        }
+
+        return bl;
+    }
+
+    @Inject(method = "tickMovement", at = @At("TAIL"))
+    void tickMovement(CallbackInfo ci) {
+        if (!this.getWorld().isClient) {
+            this.tickAngerLogic((ServerWorld) this.getWorld(), true);
+        }
+    }
+}
