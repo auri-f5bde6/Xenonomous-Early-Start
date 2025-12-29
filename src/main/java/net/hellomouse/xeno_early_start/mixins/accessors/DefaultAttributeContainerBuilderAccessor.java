f238a41364cb128dd101306b2d843902c473b1ee
Auri <f5bde6@proton.me>
2025-12-29 21:55:21 +0000

    Faster fish

---
+++ src/main/java/net/hellomouse/xeno_early_start/mixins/accessors/DefaultAttributeContainerBuilderAccessor.java
@@ -0,0 +1,15 @@
+package net.hellomouse.xeno_early_start.mixins.accessors;
+
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(DefaultAttributeContainer.Builder.class)
+public interface DefaultAttributeContainerBuilderAccessor {
+    @Accessor("instances")
+    Map<EntityAttribute, EntityAttributeInstance> xeno_early_start$getInstances();
+}
