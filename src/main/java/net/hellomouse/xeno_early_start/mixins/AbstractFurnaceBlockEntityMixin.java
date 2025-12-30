682b33368c6c7ad451fab4090ab1a00dda78025c
Auri <f5bde6@proton.me>
2025-12-30 01:06:40 +0000

    Brick furnace cannot smelt iron ore

--- src/main/java/net/hellomouse/xeno_early_start/mixins/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/net/hellomouse/xeno_early_start/mixins/AbstractFurnaceBlockEntityMixin.java
@@ -1,5 +1,7 @@
 package net.hellomouse.xeno_early_start.mixins;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.hellomouse.xeno_early_start.block.block_entity.BrickFurnaceBlockEntity;
 import net.hellomouse.xeno_early_start.mixins.accessors.AbstractFurnaceEntityAccessor;
@@ -7,12 +9,11 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.LockableContainerBlockEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.recipe.AbstractCookingRecipe;
-import net.minecraft.recipe.RecipeInputProvider;
-import net.minecraft.recipe.RecipeUnlocker;
+import net.minecraft.recipe.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -22,6 +23,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
 
 import static net.hellomouse.xeno_early_start.registries.ProgressionModDamageSource.FURNACE_EXPLOSIONS;
 import static net.hellomouse.xeno_early_start.registries.ProgressionModDamageSource.getDamageSource;
@@ -49,5 +52,20 @@ public abstract class AbstractFurnaceBlockEntityMixin extends LockableContainerB
             itemstack.setCount(itemstack.getCount() - 1);
             world.createExplosion(null, getDamageSource(FURNACE_EXPLOSIONS, world.getRegistryManager()), new ExplosionBehavior(), Vec3d.of(pos), 3f, true, World.ExplosionSourceType.BLOCK);
         }
+    }
+
+    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/recipe/RecipeManager$MatchGetter;getFirstMatch(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/world/World;)Ljava/util/Optional;"))
+    private static <C extends Inventory, T extends Recipe<C>> Optional<T> dontSmeltIron(RecipeManager.MatchGetter instance, C c, World world, Operation<Optional<T>> original) {
+        var result = original.call(instance, c, world);
+        if (c instanceof BrickFurnaceBlockEntity) {
+            if (result.isPresent()) {
+                var r = result.get();
+                var stack = r.getOutput(world.getRegistryManager());
+                if (stack.isOf(Items.IRON_INGOT) || stack.isOf(Items.IRON_NUGGET)) {
+                    return Optional.empty();
+                }
+            }
+        }
+        return result;
     }
 }
