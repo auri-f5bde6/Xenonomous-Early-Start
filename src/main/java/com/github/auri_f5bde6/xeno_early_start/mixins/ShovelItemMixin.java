854e2e44db4e2317eb8378bd70beb318457abca0
Auri <f5bde6@proton.me>
2026-01-01 01:48:11 +0000

    Proper shovel damage and play animation

--- src/main/java/com/github/auri_f5bde6/xeno_early_start/mixins/ShovelItemMixin.java
+++ src/main/java/com/github/auri_f5bde6/xeno_early_start/mixins/ShovelItemMixin.java
@@ -1,29 +1,30 @@
 package com.github.auri_f5bde6.xeno_early_start.mixins;
 
 import com.github.auri_f5bde6.xeno_early_start.block.PrimitiveFireBlock;
-import com.llamalad7.mixinextras.expression.Definition;
-import com.llamalad7.mixinextras.expression.Expression;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.github.auri_f5bde6.xeno_early_start.registries.XenoEarlyStartBlockRegistry;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.CampfireBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.ShovelItem;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ShovelItem.class)
 public class ShovelItemMixin {
-    @Definition(id = "CampfireBlock", type = CampfireBlock.class)
-    @Definition(id = "getBlock", method = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;")
-    @Expression("?.getBlock() instanceof CampfireBlock")
-    @WrapOperation(method = "useOnBlock", at = @At("MIXINEXTRAS:EXPRESSION"))
-    boolean extinguishPrimitiveFire(Object object, Operation<Boolean> original, @Local World world, @Local BlockPos pos, @Local(ordinal = 0) BlockState state) {
-        if (object instanceof PrimitiveFireBlock) {
+    @Inject(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getToolModifiedState(Lnet/minecraft/item/ItemUsageContext;Lnet/minecraftforge/common/ToolAction;Z)Lnet/minecraft/block/BlockState;"), cancellable = true)
+    void extinguishPrimitiveFire(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir, @Local World world, @Local BlockPos pos, @Local(ordinal = 0) BlockState state, @Local PlayerEntity player) {
+        if (state.isOf(XenoEarlyStartBlockRegistry.PRIMITIVE_FIRE.get())) {
             world.setBlockState(pos, state.with(PrimitiveFireBlock.LIT, false));
+            if (player != null && !player.isCreative()) {
+                context.getStack().damage(1, player, p -> p.sendToolBreakStatus(context.getHand()));
+            }
+            cir.setReturnValue(ActionResult.success(world.isClient));
         }
-        return original.call(object);
     }
 }
