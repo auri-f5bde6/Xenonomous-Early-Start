a046530b8411e95fbcdc08daccbd4349c30540d5
Auri <f5bde6@proton.me>
2026-02-18 20:45:13 +0000

    Attacking villager also anger surrounding iron golem

--- src/main/kotlin/com/github/auri_f5bde6/xeno_early_start/event/LivingAttackEventHandler.kt
+++ src/main/kotlin/com/github/auri_f5bde6/xeno_early_start/event/LivingAttackEventHandler.kt
@@ -2,6 +2,10 @@ package com.github.auri_f5bde6.xeno_early_start.event
 
 import com.github.auri_f5bde6.xeno_early_start.XenoEarlyStart
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.passive.IronGolemEntity
+import net.minecraft.entity.passive.VillagerEntity
+import net.minecraft.util.math.Box
+import net.minecraft.village.VillagerProfession
 import net.minecraftforge.event.entity.living.LivingAttackEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
@@ -21,6 +25,14 @@ object LivingAttackEventHandler {
                 ) {
                     attacked.setOnFireFor(2 * f.toInt())
                 }
+            }
+            if (attacked is VillagerEntity && attacked.villagerData.profession != VillagerProfession.NITWIT && attacker is LivingEntity) {
+                attacked.world.getEntitiesByClass(
+                    IronGolemEntity::class.java,
+                    Box(attacked.blockPos.add(-50, -50, -50), attacked.blockPos.add(50, 50, 50))
+                ) { golemEntity ->
+                    golemEntity.attacker = attacker; false
+                } // finger crossed it doesn't cause too much lag
             }
         }
 
