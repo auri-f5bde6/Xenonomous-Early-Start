901071e009e32d285665bceafde605d03bd4c973
Auri <f5bde6@proton.me>
2026-03-13 00:50:36 +0000

    Burning skeleton shoot burning arrow

---
+++ src/main/java/com/github/auri_f5bde6/xeno_early_start/mixins/mob_changes/AbstractSkeletonEntityMixin.java
@@ -0,0 +1,22 @@
+package com.github.auri_f5bde6.xeno_early_start.mixins.mob_changes;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.mob.AbstractSkeletonEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(AbstractSkeletonEntity.class)
+public class AbstractSkeletonEntityMixin {
+    @Definition(id = "spawnEntity", method = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z")
+    @Expression("?.spawnEntity(@(?))")
+    @ModifyExpressionValue(method = "attack", at = @At(value = "MIXINEXTRAS:EXPRESSION"))
+    PersistentProjectileEntity maybeSetOnFire(PersistentProjectileEntity original) {
+        if (((AbstractSkeletonEntity) (Object) this).isOnFire()) {
+            original.setOnFire(true);
+        }
+        return original;
+    }
+}
